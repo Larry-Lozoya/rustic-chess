6adcd43a9acada62c441177a015503ccdd5d84b9
Larry-Lozoya <167731449+Larry-Lozoya@users.noreply.github.com>
2024-12-17 21:15:01 -0700

    added the systems.rs
    Added the systems.rs file to make code a little more readable and to not have it in the main and rather as an actual system.

--- rustic_chess/src/main.rs
+++ rustic_chess/src/main.rs
@@ -4,11 +4,13 @@ use bevy::{prelude::*, transform::commands, winit::WinitSettings,color::palettes
 use bevy_mod_picking::*;
 use components::{Pawn, Peices};
 use setup::{chess_board, setup, setupPieces,mouse_button_events, mouse_button_location};
+use systems::button_system;
 use plugins::*;
 
 mod setup;
 mod plugins;
 mod components;
+mod systems;
     /*
         Link for mouse events:
         https://docs.rs/winit_input_helper/0.15.3/src/winit_input_helper/winit_input_helper.rs.html#312-324
@@ -37,64 +39,6 @@ fn main() {
         //.add_systems(Update, print_mouse.run_if(resource_changed::<ButtonInput<MouseButton>>),)
         .run();
 }
-
-const NORMAL_BUTTON: Color = Color::srgb(0.15, 0.15, 0.15);
-const HOVERED_BUTTON: Color = Color::srgb(0.25, 0.25, 0.25);
-const PRESSED_BUTTON: Color = Color::srgb(0.35, 0.75, 0.35);
-
-pub fn button_system(
-    mut interaction_query: Query<
-        (
-            &Interaction,
-            &Name,
-            &mut BackgroundColor,
-            &mut BorderColor,
-        ),
-        (Changed<Interaction>, With<Button>),
-    >,
-    mut pawn_query: Query<(&mut Transform, &Peices), With<Pawn>>,
-) {
-    for (interaction, name, mut color, mut border_color) in &mut interaction_query {
-        match *interaction {
-            Interaction::Pressed => {
-                if name.as_str()== "MoveWhitePawnButton" {
-                    println!("DID we press the button");
-                    for (mut transform, peice) in &mut pawn_query {
-                        if let Peices::Pawn(ref pawn_color, ref pawn_num) = peice {
-                            if pawn_color == "white" && *pawn_num == 1.0{
-                                transform.translation.y += 64.0;
-                                println!("Moving pawn: {}", pawn_color);
-                                break;
-                            }
-                        }
-                    }
-                }else if name.as_str() == "MoveBlackPawnButton"{
-                    println!("DID we press the button");
-                    for (mut transform, peice) in &mut pawn_query {
-                        if let Peices::Pawn(ref pawn_color, ref pawn_num) = peice {
-                            if pawn_color == "black" && *pawn_num == 2.0{
-                                transform.translation.y -= 64.0;
-                                println!("Moving pawn: {}", pawn_color);
-                                break;
-                            }
-                        }
-                    }
-                }
-                *color = Color::WHITE.into();
-            }
-            Interaction::Hovered => {
-                *color = HOVERED_BUTTON.into();
-                border_color.0 = Color::WHITE;
-            }
-            Interaction::None => {
-                //**text = "Button".to_string();
-                *color = NORMAL_BUTTON.into();
-                border_color.0 = Color::BLACK;
-            }
-        }
-    }
-}
-
 
 
 
